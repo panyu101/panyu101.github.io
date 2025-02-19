fa8b5f2e839afdd802bff03db4692364e573fdbc
panyu101 <52611239+panyu101@users.noreply.github.com>
2025-02-19 17:21:55 +0000

    Deploying to gh-pages from @ panyu101/panyu101.github.io@46859f8466ab86f779ee1dc8bab86da0f37174c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-market",
+            },{id: "post-customize-post-template",
+      
+        title: "customize post template",
+      
+      description: "customize post template to remove these no need",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/customize-post-template/";
+        
+      },
+    },{id: "post-market",
       
         title: "market",
       
